7029610da9fc8b2652cfd31aa61c14e2ca67abaf
SandeepM <dev13.ultroneous@gmail.com>
2022-10-28 14:31:55 +0530

    populate reviews for one customer, serviceprovider

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,24 +1,62 @@
-const Review = require("../models/reviewModel")
-const Customer = require("../models/customerModel")
-const catchAsyncError = require("../utils/catchAsyncError")
-const { sendResponse } = require("../utils/commonFunctions")
-const ServiceProvider = require("../models/serviceProviderModel")
-const mongoose = require("mongoose")
-const AppError = require("../utils/appError")
-const { errorMessages } = require("../utils/messages")
-
+const Review = require("../models/reviewModel");
+const Customer = require("../models/customerModel");
+const catchAsyncError = require("../utils/catchAsyncError");
+const { sendResponse } = require("../utils/commonFunctions");
+const ServiceProvider = require("../models/serviceProviderModel");
+const mongoose = require("mongoose");
+const AppError = require("../utils/appError");
+const { errorMessages } = require("../utils/messages");
 
 exports.getAllCustomers = catchAsyncError(async (req, res, next) => {
-  const customers = await Customer.find().sort("-updatedAt");
+  const { id } = req.body;
 
-  sendResponse(customers, 200, res);
+  let users;
+
+  if (id) {
+    // get reviews by customer
+    users = await Customer.findById(id)
+      .populate({
+        path: "reviews",
+        select: "review overallRating",
+        populate: {
+          path: "serviceProviderId",
+          select: "name",
+        },
+      })
+      .select("name email contact overallRating totalReviews");
+  } else {
+    users = await Customer.find().sort("-updatedAt");
+  }
+
+  sendResponse(users, 200, res);
 });
 
 exports.getAllServiceProviders = catchAsyncError(async (req, res, next) => {
   let currentUser = req.user._id;
-  const serviceProviders = await ServiceProvider.find({ "_id": { "$ne": currentUser } }).sort("-updatedAt");
 
-  sendResponse(serviceProviders, 200, res);
+  const { id } = req.body;
+
+  let users;
+
+  if (id) {
+    // get reviews by serviceprovider
+    users = await ServiceProvider.findById(id)
+      .populate({
+        path: "reviews",
+        select: "review overallRating",
+        populate: {
+          path: "customerId",
+          select: "name",
+        },
+      })
+      .select("name email contact overallRating totalReviews");
+  } else {
+    users = await ServiceProvider.find({
+      _id: { $ne: currentUser },
+    }).sort("-updatedAt");
+  }
+
+  sendResponse(users, 200, res);
 });
 
 exports.addQuestion = catchAsyncError(async (req, res, next) => {
@@ -29,20 +67,57 @@ exports.addQuestion = catchAsyncError(async (req, res, next) => {
     details,
   });
 
-  sendResponse(serviceProviders, 200, res)
-})
+  sendResponse(serviceProviders, 200, res);
+});
 
 exports.blockUnblockUser = catchAsyncError(async (req, res, next) => {
-  const { userId } = req.body
+  const { userId } = req.body;
 
   if (!mongoose.isValidObjectId(userId)) {
-    return next(
-      new AppError(401, errorMessages.other.userblock)
-    );
+    return next(new AppError(401, errorMessages.other.userblock));
   }
 
-  const checkBlocking = await ServiceProvider.findOne({ _id: userId })
-  const updatedData = await ServiceProvider.findByIdAndUpdate({ _id: userId }, { isActive: !checkBlocking.isActive })
+  const checkBlocking = await ServiceProvider.findOne({ _id: userId });
+  const updatedData = await ServiceProvider.findByIdAndUpdate(
+    { _id: userId },
+    { isActive: !checkBlocking.isActive }
+  );
 
-  sendResponse(updatedData, 200, res)
-})
+  sendResponse(updatedData, 200, res);
+});
+
+exports.search = catchAsyncError(async (req, res, next) => {
+  const { searchText, searchField } = req.body;
+
+  let Model = Customer;
+  if (searchField && searchField === "ServiceProvider") {
+    Model = ServiceProvider;
+  }
+
+  const results = await Model.find({
+    $or: [
+      {
+        name: {
+          $regex: searchText,
+          $options: "i",
+        },
+      },
+      {
+        email: {
+          $regex: searchText,
+          $options: "i",
+        },
+      },
+      {
+        contact: {
+          $regex: searchText,
+          $options: "i",
+        },
+      },
+    ],
+  })
+    .select("name email contact")
+    .limit(5);
+
+  sendResponse(results, 200, res);
+});
